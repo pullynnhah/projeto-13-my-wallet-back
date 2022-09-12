e985f1c076e6a06aa66fb0c8a5205876506da3fb
Paula Rodrigues <paulinhaabro@gmail.com>
2022-09-12 03:23:57 -0300

    edit schema

--- src/schemas/wallet.schema.js
+++ src/schemas/wallet.schema.js
@@ -1,7 +1,7 @@
 import joi from "joi";
 
 const walletSchema = joi.object({
-  amount: joi.number().positive().precision(2).required(),
+  amount: joi.number().positive().required(),
   description: joi.string().required(),
   type: joi.valid("income", "expense").required(),
 });
