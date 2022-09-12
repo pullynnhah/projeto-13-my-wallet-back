8cf08cfdb0e20fbf291367d6ffeee2126b495ddf
Paula Rodrigues <paulinhaabro@gmail.com>
2022-09-11 22:15:47 -0300

    fix :bug:

--- src/controllers/wallet.controller.js
+++ src/controllers/wallet.controller.js
@@ -1,5 +1,6 @@
 import dayjs from "dayjs";
 import {db} from "../database/mongo.db.js";
+import {ObjectId} from "mongodb";
 
 const listTransactions = async (req, res) => {
   const {userId} = res.locals;
@@ -13,23 +14,30 @@ const listTransactions = async (req, res) => {
 const createTransaction = async (req, res) => {
   const {data, userId} = res.locals;
   try {
-    await db.collection("wallets").insertOne({...data, userId, date: dayjs().format("DD/MM")});
-    res.sendStatus(201);
+    const {insertedId} = await db
+      .collection("wallets")
+      .insertOne({...data, userId, date: dayjs().format("DD/MM")});
+    res.status(201).send({walletId: insertedId});
   } catch (e) {
     res.sendStatus(500);
   }
 };
 
 const editTransaction = async (req, res) => {
-  const {walletId} = req.headers;
+  const {wallet} = req.headers;
   const {data} = res.locals;
 
-  if (!walletId) {
+  if (!wallet) {
     return res.sendStatus(400);
   }
 
   try {
-    await db.collection("wallets").updateOne({_id: walletId}, {$set: data});
+    const wallets = await db.collection("wallets").findOne({_id: ObjectId(wallet)});
+    console.log(wallets);
+    const {modifiedCount} = await db
+      .collection("wallets")
+      .updateOne({_id: ObjectId(wallet)}, {$set: data});
+    console.log(modifiedCount);
     res.sendStatus(201);
   } catch (e) {
     res.sendStatus(500);
@@ -37,14 +45,14 @@ const editTransaction = async (req, res) => {
 };
 
 const removeTransaction = async (req, res) => {
-  const {walletId} = req.headers;
+  const {wallet} = req.headers;
 
-  if (!walletId) {
+  if (!wallet) {
     return res.sendStatus(400);
   }
 
   try {
-    await db.collection("wallets").deleteOne({_id: walletId});
+    await db.collection("wallets").deleteOne({_id: wallet});
   } catch (e) {
     res.sendStatus(500);
   }
